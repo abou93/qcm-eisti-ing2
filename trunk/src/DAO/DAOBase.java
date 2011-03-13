1c563f8a0734f2ef2128c924ebca9e29cad0cc26
mika.23.joo@gmail.com <mika.23.joo@gmail.com@c3d2129a-06be-aff8-f36c-a57d28f9f80a>
2011-03-13 18:04:23 +0000

    Avec tous les fichiers c'est mieux :)

---
+++ trunk/src/DAO/DAOBase.java
@@ -0,0 +1,49 @@
+package DAO;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import DAO.HibernateUtil;
+import Modele.Cours;
+import Modele.QCM;
+
+
+public class DAOBase 
+{	
+
+	@SuppressWarnings("unchecked")
+	public static List<Cours> getListCours()
+	{
+		Session session= HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		List<Cours> result = session.createQuery("from Cours").list();
+		session.close();
+		return result;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<QCM> getListQCM()
+	{
+		Session session= HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		List<QCM> result = session.createQuery("from QCM").list();
+		session.close();
+		return result;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<QCM> getMesQCMs(Cours c)
+	{
+		Session session= HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		List<QCM> result = session.createQuery("from QCM where id_cours = "+c.getId()).list();
+		System.out.println("result : "+result.size() );
+//		List<QCM> result = session.createQuery("from QCM where id = ALL("+
+//				"select id from Cours where nom = \'"+c.getNom()+"\')")
+//				.list();
+		
+		session.close();
+		return result;
+	}
+}
