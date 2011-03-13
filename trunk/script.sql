50ceaa8aea5cf5576474d71a9d10ce7281f5afc3
mika.23.joo@gmail.com <mika.23.joo@gmail.com@c3d2129a-06be-aff8-f36c-a57d28f9f80a>
2011-03-13 18:16:33 +0000

    script.sql pour les tables Utilisateur, Cours et QCM

--- trunk/script.sql
+++ trunk/script.sql
@@ -1,3 +1,5 @@
+DROP TABLE Utilisateur CASCADE CONSTRAINT;
+
 CREATE TABLE Utilisateur (
 	user_id NUMBER CONSTRAINT pk_utilisateur PRIMARY KEY,
 	user_login VARCHAR2(50),
@@ -8,3 +10,28 @@ INSERT INTO Utilisateur VALUES (1, 'rachou', '123', 0);
 INSERT INTO Utilisateur VALUES (2, 'stitch', '456', 1);
 INSERT INTO Utilisateur VALUES (3, 'michou', '789', 1);
 INSERT INTO Utilisateur VALUES (4, 'fransou', '000', 0);
+
+
+
+/*-------------------------------------------------*/
+
+DROP TABLE Cours CASCADE CONSTRAINT;
+DROP TABLE Qcm CASCADE CONSTRAINT;
+
+CREATE TABLE Qcm (
+	id NUMBER(4) CONSTRAINT pk_qcm PRIMARY KEY,
+	qcmXML XMLTYPE,
+  id_cours NUMBER(4)
+	);
+	
+CREATE TABLE Cours (
+	id NUMBER(4) CONSTRAINT pk_cours PRIMARY KEY,
+	nom VARCHAR2(50)
+	);	
+	
+ALTER TABLE Qcm ADD CONSTRAINT fk_qcm_cours FOREIGN KEY (id_cours) REFERENCES Cours (id);  
+
+
+/** Insertion Cours **/
+INSERT INTO Cours (ID, NOM) VALUES ('1', 'Droit');
+INSERT INTO Cours (ID, NOM) VALUES ('2', 'HTML');
